eb98386689a103d82bea347e7a59e070f742d7fb
harhogefoo <harimanmon@gmail.com>
2017-02-25 23:23:06 +0900

    solve abc054b

--- ABC054/b.rb
+++ ABC054/b.rb
@@ -1,7 +1,39 @@
+def create(num)
+  Array.new(num){ Array.new(num) }
+end
+
+def input(array, num)
+  num.times do |i|
+    input = gets.chomp.split('')
+    input.each_with_index do |c, j|
+      array[i][j] = c
+    end
+  end
+end
 
 n, m = gets.chomp.split(' ').map(&:to_i)
 
-n.times do
-n.each do |i|
+a = create(n)
+b = create(m)
+input(a, n)
+input(b, m)
 
+def check(a, b, i, j)
+  b.length.times do |k|
+    b.length.times do |l|
+      return false if a[i+k][j+l] != b[k][l]
+    end
+  end
+  true
 end
+
+(n-m+1).times do |i|
+  (n-m+1).times do |j|
+    is_match = check(a, b, i, j)
+    if is_match
+      puts 'Yes'
+      exit
+    end
+  end
+end
+puts 'No'
